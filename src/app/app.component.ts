78757270dfc6d79fabd0704929cc9c690c803b9d
TARIQ ALHARBI <rescco14@gmail.com>
2024-04-07 04:45:57 +0300

    hehen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'salts';
+
+  options = ['Daily', 'Weekly', 'Monthly'];
+
+  handleIndexChange(e: number): void {
+    console.log(e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ar_EG } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ar from '@angular/common/locales/ar';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
+
+registerLocaleData(ar);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
+    NzButtonModule,
+    NzSegmentedModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: ar_EG }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
